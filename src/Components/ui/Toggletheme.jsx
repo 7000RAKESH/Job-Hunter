dbd5c92ed0601c8a4358f85b4806d7f3ed3f3dc6
7000RAKESH <rakeshmca222@gmail.com>
2025-03-17 14:58:57 +0530

    yes

--- src/Components/ui/Toggletheme.jsx
+++ src/Components/ui/Toggletheme.jsx
@@ -1,13 +1,13 @@
 import { Moon, Sun } from "lucide-react";
 
-import { Button } from "@/components/ui/button";
+import { Button } from "@/Components/ui/button";
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import { useTheme } from "@/components/ui/Themeprovider";
+} from "@/Components/ui/dropdown-menu";
+import { useTheme } from "@/Components/ui/Themeprovider";
 
 export function Toggletheme() {
   const { setTheme } = useTheme();
